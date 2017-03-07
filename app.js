e23edd7c466c3dae6249f6af4aeddb80ce49dfac
davies <teehanong2@gmail.com>
2017-03-07 16:37:24 +0800

    fix code

--- app.js
+++ app.js
@@ -1,66 +1,66 @@
-// app.js
 
-// BASE SETUP
-// =============================================================================
+/**
+ * Module dependencies.
+ */
 
-// call the packages we need
-var express = require('express');
-var cors = require('cors');
+var express = require('express')
+    , http = require('http')
+    , path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var methodOverride = require('method-override');
+var bodyParser = require('body-parser');
 var app = express();
 
-var bodyParser = require('body-parser');
-var firebase = require('firebase');
-var moment = require('moment');
-var https = require('https');
-var request = require('request');
 
-app.use(bodyParser.urlencoded({ extended: true }));// support encoded bodies
-app.use(bodyParser.json()); //Support JSON encoded bodies
-app.use(cors());//Enable cors for all routes
-var port = process.env.VCAP_APP_PORT || 3000;
+// Listening to port 3000 when nodeJs server is running on localhost
+app.set('port', process.env.PORT || 3000);
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(methodOverride());
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+// error handler
 
-// ROUTES FOR OUR API
-// =============================================================================
-var router = express.Router();              // get an instance of the express Router
 
-// middleware to use for all requests
-router.use(function(req, res, next) {
-    // do logging
-    console.log('Something has happened.');
-    next(); // make sure we go to the next routes and don't stop here
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
-app.route('/API/MathManager/addTwoNumbers')
-    .post(function(req, res) {
-        var number1 = req.body.number1;
-        var number2 = req.body.number2;
-        res.json({ result: (number1 + number2  ) });
-    });
-app.route('/API/MathManager/divideTwoNumbers')
-    .post(function(req, res) {
-        var number1 = req.body.number1;
-        var number2 = req.body.number2;
-        res.json({ result: (number1 / number2) });
-    });
-app.route('/API/MathManager/multiplyTwoNumbers')
-    .post(function(req, res) {
-        var number1 = req.body.number1;
-        var number2 = req.body.number2;
-        res.json({ result: (number1 * number2) });
-    });
-app.route('/API/MathManager/calculateDifferencesBetweenTwoNumbers')
-    .post(function(req, res) {
-        var number1 = req.body.number1;
-        var number2 = req.body.number2;
-        res.json({ result: Math.abs(number1 - number2) });
-    });
-
-router.get('api/', function(req, res) {
-    res.json({ result: 'Route: \'/\' Math Manager Version 1' });
-
+//http://stackoverflow.com/questions/32957123/express-router-crud-api-cannot-delete
+app.all('*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authorization');
+    next();
 });
+/* Importing the function from index.js and parsing app(express instance).
+ By doing so, you are able to require the files in the controllers folder.
+ Refer to index.js to find out about the files that are being required.
+ */
+require("./routes/index")(app);
 
-// START THE SERVER
-// =============================================================================
-app.listen(port);
-console.log('Magic happens on port ' + port);
+
+
+
+
+// Initializing the server when server.js is being executed
+http.createServer(app).listen(app.get('port'), function(){
+    console.log("Express server listening on port " + app.get('port'));
+});
